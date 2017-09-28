09a3591fdc7a88d8e079956b7d090b631f6d08c9
Rashi Bose <rbose@Rashis-MacBook-Pro.local>
2017-09-28 08:08:34 -0700

    Initial Commit

---
+++ StackViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewExample
+//
+//  Created by Rashi Bose on 9/28/17.
+//  Copyright © 2017 Rashi Bose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
